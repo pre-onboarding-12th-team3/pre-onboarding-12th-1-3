5bc196d3d10809728e0a537b08f8a54474b2dd70
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-08-24 03:25:52 +0900

    [Feat] todo 목록 추가
    - todo 목록 추가
    - todo 아이템 컴포넌트 추가

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import useAPI from '@/apis/todo/todo';
+import TodoList from '@/components/domain/todo/TodoList';
+
+interface Todo {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
+
 const TodoPage = () => {
-  return <div>TodoPage</div>;
+  const [todoList, setTodoList] = useState<Todo[]>([]); 
+  const {getTodos} = useAPI();
+  
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const fetchedTodos = await getTodos();
+        setTodoList(fetchedTodos);
+      } catch (error) {
+        window.alert('할 일 목록을 불러오는 데 실패했습니다');
+      }
+    }
+    fetchTodos();
+  }, []);
+
+  return (
+    <TodoList items={todoList}/>
+  );
 };
 
 export default TodoPage;
