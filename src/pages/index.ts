0da5a8572f4a81bd743fc3cb2a95a225e4141cf0
ukssss <1872003115su@gmail.com>
2023-08-24 00:26:18 +0900

    [Feat] HomePage 페이지 추가
    - 기본 경로("/") 접속시 렌더링 되어야 할 페이지
    - token 여부에 따라 "/signin" 혹은 "/todo"로 이동해야 하기에 실제로 출력되지는 않는 페이지

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as ErrorPage } from './ErrorPage';
 export { default as NotFoundPage } from './NotFoundPage';
+export { default as HomePage } from './HomePage';
 export { default as SignInPage } from './SignInPage';
 export { default as SignUpPage } from './SignUpPage';
 export { default as TodoPage } from './TodoPage';
