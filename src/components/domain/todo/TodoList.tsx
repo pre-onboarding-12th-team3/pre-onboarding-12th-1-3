9efbaa91f33393553e956d74d231e20b37cb7313
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-08-25 01:04:25 +0900

    [Feat] todo 수정, 삭제 기능 구현
    - todo 수정 기능 구현
    - todo 삭제 기능 구현

--- src/components/domain/todo/TodoList.tsx
+++ src/components/domain/todo/TodoList.tsx
@@ -1,3 +1,4 @@
+import useAPI from "@/apis/todo/todo";
 import TodoItem from "./TodoItem";
 
 interface Todo {
@@ -8,13 +9,48 @@ interface Todo {
 }
 
 interface Props {
-    items: Todo[];
+    items: Todo[],
+    setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList = ({ items }: Props) => {
+const TodoList = ({ items, setTodoList }: Props) => {
+    const {updateTodo, deleteTodo} = useAPI();
+
+    const handleUpdate = async(
+        id: number,
+        todo: string,
+        isCompleted: boolean
+    ) => {
+        const prevTodo = items.find(todo => todo.id === id);
+        if(!prevTodo || prevTodo.todo === todo && prevTodo.isCompleted === isCompleted) return;
+        try {
+            await updateTodo(id, todo, isCompleted);
+            setTodoList(prev => 
+                prev.map(item => 
+                    item.id === id ? {
+                        ...item,
+                        todo,
+                        isCompleted
+                    } : item
+                )
+            )
+        } catch(err) {
+            alert('할 일을 수정하는 데 실패했습니다');
+        }
+    }
+
+    const handleDelete = async(id: number) => {
+        try {
+            await deleteTodo(id);
+            setTodoList(prev => prev.filter(item => item.id !== id));
+        } catch(err) {
+            alert('할 일을 삭제하는 데 실패했습니다');
+        }
+    }
+
     return (
         <ul>
-            {items.map(item => <TodoItem key={item.id} item={item}/>)}
+            {items.map(item => <TodoItem key={item.id} item={item} onUpdate={handleUpdate} onDelete={handleDelete}/>)}
         </ul>
     );
 }
