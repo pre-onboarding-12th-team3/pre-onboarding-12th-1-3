6d03ebac1a0ce96e659bad493a561107cdd5f80c
ShinJaeIl01 <ekvm555@naver.com>
2023-08-24 21:43:52 +0900

    [type] 로그인 Api Response Type 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -4,12 +4,15 @@ interface AuthProps {
   email: string;
   password: string;
 }
+interface SignInResponse {
+  access_token: string;
+}
 
 const signUpApi = async ({ email, password }: AuthProps) => {
   return await axiosInstance.post('/auth/signup', { email, password });
 };
 const signInApi = async ({ email, password }: AuthProps) => {
-  return await axiosInstance.post('/auth/signin', { email, password });
+  return await axiosInstance.post<SignInResponse>('/auth/signin', { email, password });
 };
 
 export { signUpApi, signInApi };
