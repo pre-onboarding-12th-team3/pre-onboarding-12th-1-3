e7050a8eb3afd3f7817360e55a2723c2731db548
ukss <1872003115su@gmail.com>
2023-08-25 17:07:36 +0900

    Revert "[Feat] 페이지 라우팅 설정 및 RootLayout 제작"

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,3 @@
-export { default as ProtectedRoute } from './ProtectedRoute';
-export { default as RootLayout } from './RootLayout';
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as Heading } from './Heading';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 export { default as ErrorPage } from './ErrorPage';
 export { default as NotFoundPage } from './NotFoundPage';
-export { default as HomePage } from './HomePage';
 export { default as SignInPage } from './SignInPage';
 export { default as SignUpPage } from './SignUpPage';
 export { default as TodoPage } from './TodoPage';
